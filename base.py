b40ddb5f7eb81eeafd280425f4b50fe38c5a2f80
Shampad Sharkar <sompodsign@gmail.com>
2022-07-04 03:29:33 +0600

    base class seperated

--- base.py
+++ base.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from driver_actions import DriverActions
 
 
 class Scrape:
+
+    OPTIONS = Options()
+    OPTIONS.add_argument('--headless')
+
     def __init__(self, url):
         self.url = url
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.OPTIONS)
         self.driver.maximize_window()
         self.driver.get(url)
         self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
@@ -24,6 +29,7 @@ class Scrape:
         return self.driver.title
 
     def get_html(self):
+        self.driver_actions = DriverActions(self.driver)
         self.soup = self.parse_html(self.driver.page_source)
 
     def close(self):
