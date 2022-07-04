070c6ac2a54fad27e2f725a009048a2ca24a693e
Shampad Sharkar <sompodsign@gmail.com>
2022-07-05 02:03:28 +0600

    create extractor functionality for tags

--- digital_ocean/ocean_content_scraper.py
+++ digital_ocean/ocean_content_scraper.py
@@ -1,5 +1,4 @@
 import re
-
 import pandas as pd
 
 from bs_base import BaseSoupScraper
@@ -8,9 +7,11 @@ from bs_base import BaseSoupScraper
 def get_content(link):
     tutorial_soup = BaseSoupScraper(link)
     tutorial_soup.get_html()
+    tags = tutorial_soup.soup.find_all('a', class_=re.compile('TagStyles__StyledTag'))
+    tags = [tag.text for tag in tags]
     title = tutorial_soup.soup.find('h1', class_=re.compile('HeadingStyles__StyledH1')).text
     content = tutorial_soup.soup.find('div', class_=re.compile('Markdown_markdown__'))
-    return {'title': title, 'content': content}
+    return {'title': title, 'content': content, 'tags': tags}
 
 
 def get_tutorial_links(csv):
